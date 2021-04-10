65d92b6b033418bbd1a97aea0d3e5e8d64698ee2
Jack Shell <jackshel45@gmail.com>
2021-04-11 00:50:43 +0300

    Added all square angles

--- src/Editor.js
+++ src/Editor.js
@@ -7,7 +7,17 @@ export class Editor extends React.Component {
     super( props );
     this.state =
     {
-      states : [],
+      initialized : false,
+      states :
+      [
+        // initial state of the room, this points will have handlers for adding angles and cutouts
+        [
+          { x: 1000 * 0.05, y: 1000 * 0.95, order : 0 },
+          { x: 1000 * 0.05, y: 1000 * 0.05, order : 1 },
+          { x: 1000 * 0.95, y: 1000 * 0.05, order : 2 },
+          { x: 1000 * 0.95, y: 1000 * 0.95, order : 3 }
+        ],
+      ],
       action : 'no', /* triag, square, cut */
       isReceivedWidth : false,
       isReceivedDepth : false,
@@ -25,10 +35,11 @@ export class Editor extends React.Component {
       //   { x: 1000, y: 1000 },
       [
         // use width and depth
-        { x: 1000 * 0.05, y: 1000 * 0.95 },
-        { x: 1000 * 0.05, y: 1000 * 0.05 },
-        { x: 1000 * 0.95, y: 1000 * 0.05 },
-        { x: 1000 * 0.95, y: 1000 * 0.95 },
+        // only points with order properties can be dragged
+        { x: 1000 * 0.05, y: 1000 * 0.95, order : 0 },
+        { x: 1000 * 0.05, y: 1000 * 0.05, order : 1 },
+        { x: 1000 * 0.95, y: 1000 * 0.05, order : 2 },
+        { x: 1000 * 0.95, y: 1000 * 0.95, order : 3 },
       ]
     };
 
@@ -49,13 +60,12 @@ export class Editor extends React.Component {
 
   calculateNewCoordinates( state )
   {
-    debugger;
     return (
       [
-        { x: state.width * 0.05, y: state.depth * 0.95 },
-        { x: state.width * 0.05, y: state.depth * 0.05 },
-        { x: state.width * 0.95, y: state.depth * 0.05 },
-        { x: state.width * 0.95, y: state.depth * 0.95 },
+        { x: state.width * 0.05, y: state.depth * 0.95, order : 0 },
+        { x: state.width * 0.05, y: state.depth * 0.05, order : 1 },
+        { x: state.width * 0.95, y: state.depth * 0.05, order : 2 },
+        { x: state.width * 0.95, y: state.depth * 0.95, order : 3 },
       ]
     )
   }
@@ -66,6 +76,7 @@ export class Editor extends React.Component {
     // push current state to the states array
 
     debugger
+    // const points = this.state.states[ 0 ];
     const points = this.state.roomCoordinates;
     const width = this.state.width;
     const depth = this.state.depth;
@@ -73,6 +84,7 @@ export class Editor extends React.Component {
     let pointsStr = points.map( ( el ) => `${el.x},${el.y}` ).join( ' ' );
     console.log( pointsStr );
     console.log( this.state );
+    console.log( this.state.roomCoordinates.toString() === this.state.states[ 0 ].toString() );
 
     /* Buttons */
 
@@ -188,7 +200,7 @@ export class Editor extends React.Component {
     {
       dots = points.map( ( el, i ) => {
         // return <rect x={ el.x - 20 } y={ el.y - 20 } width="40" height="40" rx="45" fill="grey"/>;
-        return <rect x={ el.x - ( width * 0.05 / 2 ) } y={ el.y - ( width * 0.05 / 2 ) } width={ width * 0.05 } height={ width * 0.05 } rx="45" fill="grey"/>;
+        return <rect x={ el.x - ( width * 0.04 / 2 ) } y={ el.y - ( width * 0.04 / 2 ) } width={ width * 0.04 } height={ width * 0.04 } rx="45" fill="grey"/>;
       });
       // console.log( dots )
     }
@@ -196,7 +208,7 @@ export class Editor extends React.Component {
     {
       dots = points.map( ( el, i ) =>
       {
-        return <rect x={ el.x - ( width * 0.05 / 2 ) } y={ el.y - ( width * 0.05 / 2 ) } width={ width * 0.05 } height={ width * 0.05 } fill="grey" onMouseDown={( e ) => this.dragSquareCorner( e, i, this.svgPolygon )} />;
+        return <rect x={ el.x - ( width * 0.04 / 2 ) } y={ el.y - ( width * 0.04 / 2 ) } width={ width * 0.04 } height={ width * 0.04 } fill="grey" onMouseDown={( e ) => this.dragSquareCorner( e, i, this.svgPolygon )} />;
       });
     }
     else if( this.state.action === 'cut' )
@@ -206,27 +218,27 @@ export class Editor extends React.Component {
         if( i > 0 )
         {
           // current = <rect x={ ( prevCoordinate.x + el.x ) / 2 - 25 } y={( prevCoordinate.y + el.y ) / 2 - 25 } width="50" height="50" rx="15" fill="grey"/>;
-          current = <rect x={ ( prevCoordinate.x + el.x ) / 2 - ( width * 0.05 / 2 ) } y={( prevCoordinate.y + el.y ) / 2 - ( width * 0.05 / 2 ) } width={ width * 0.05 } height={ width * 0.05 } rx="15" fill="grey"/>;
+          current = <rect x={ ( prevCoordinate.x + el.x ) / 2 - ( width * 0.04 / 2 ) } y={( prevCoordinate.y + el.y ) / 2 - ( width * 0.04 / 2 ) } width={ width * 0.04 } height={ width * 0.04 } rx="15" fill="grey"/>;
           prevCoordinate=el;
           return current;
         }
         else
         {
           // return <rect x={ ( points[ 0 ].x + points[ points.length - 1 ].x ) / 2 - 25 } y={( points[ 0 ].y + points[ points.length - 1 ].y ) / 2 - 25 } width="50" height="50" rx="15" fill="grey"/>;
-          return <rect x={ ( points[ 0 ].x + points[ points.length - 1 ].x ) / 2 - ( width * 0.05 / 2 ) } y={( points[ 0 ].y + points[ points.length - 1 ].y ) / 2 - ( width * 0.05 / 2 ) } width={ width * 0.05 } height={ width * 0.05 } rx="15" fill="grey"/>;
+          return <rect x={ ( points[ 0 ].x + points[ points.length - 1 ].x ) / 2 - ( width * 0.04 / 2 ) } y={( points[ 0 ].y + points[ points.length - 1 ].y ) / 2 - ( width * 0.04 / 2 ) } width={ width * 0.04 } height={ width * 0.04 } rx="15" fill="grey"/>;
         }
       })
     }
     
     let states = (
-      <div widht="50px"> States : {JSON.stringify( this.states )} </div>
+      <div width="50px"> States : {JSON.stringify( this.states )} </div>
     )
 
     return (
       <div className='editor'>
         <div className='interface'>
         { form }
-        { states }
+        {/* { states } */}
         </div>
         <svg width="100%" viewBox={`0 0 ${this.state.width} ${this.state.depth}`} ref={(svg) => this.svg = svg}>
           { line }
@@ -245,65 +257,50 @@ export class Editor extends React.Component {
     );
   }
 
-  // addState()
-  // {
-  //   this.setState( ( state ) =>
-  //   ({
-  //     states : [ ... state.states, state  ]
-  //   }) )
-  // }
-
   addTrianularCorner( event )
   {
     
     event.preventDefault();
     this.setState({ action : 'triag' });
-    // console.log( this.state.action )
   }
 
   addSquareCorner( event )
   {
     event.preventDefault();
     this.setState({ action : 'square' });
-    // console.log( this.state.action )
   }
 
   addCutout( event )
   {
     event.preventDefault();
     this.setState({ action : 'cut' });
-    // console.log( this.state.action )
   }
 
   /* */
 
   handleChangeWidth( event )
   {
-    debugger
     this.setState({ width : +event.target.value });
   }
 
   handleSubmitWidth( event )
   {
-    debugger
     // clear form field after it has been submitted
     event.preventDefault();
-    this.setState( ( state ) => ( { isReceivedWidth: true, roomCoordinates : this.calculateNewCoordinates( state ) } ) );
+    this.setState( ( state ) => ( { isReceivedWidth: true, roomCoordinates : this.calculateNewCoordinates( state ), states : [ this.calculateNewCoordinates( state ) ] } ) );
     // console.log( this.state );
   }
 
   handleChangeDepth( event )
   {
-    debugger
     this.setState({ depth : +event.target.value });
   }
 
   handleSubmitDepth( event )
   {
-    debugger
     // clear form field after it has been submitted
     event.preventDefault();
-    this.setState( ( state ) => ( { isReceivedDepth: true, roomCoordinates : this.calculateNewCoordinates( state ) } ) );
+    this.setState( ( state ) => ( { isReceivedDepth: true, roomCoordinates : this.calculateNewCoordinates( state ), states : [ this.calculateNewCoordinates( state ) ] } ) );
     // console.log( this.state );
   }
 
@@ -319,6 +316,7 @@ export class Editor extends React.Component {
 
   dragSquareCorner( event, index, draggedElem )
   {
+    debugger
     event.preventDefault();
     let point = this.svg.createSVGPoint();
     point.x = event.clientX;
@@ -351,12 +349,13 @@ export class Editor extends React.Component {
       // ]
     });
 
-    const mousemove = (event) =>
+    const mousemove = ( event ) =>
     {
+      // let coordinates = { roomCoordinates : [] };
       event.preventDefault();
       point.x = event.clientX;
       point.y = event.clientY;
-      let cursor = point.matrixTransform(this.svg.getScreenCTM().inverse());
+      let cursor = point.matrixTransform( this.svg.getScreenCTM().inverse() );
       this.setState( ( state ) =>
       {
 
@@ -364,16 +363,20 @@ export class Editor extends React.Component {
         // let point_y = Math.max( Math.min( cursor.y, state.width ), 0 ) - this.state.dragOffset.y;
         // let point_x = cursor.x - this.state.dragOffset.x;
         // let point_y = cursor.y - this.state.dragOffset.y;
-        // let point_x = Math.max( Math.min( cursor.x, state.width ), 0 );
-        // let point_y = Math.max( Math.min( cursor.y, state.height ), 0 );
-        let point_x = cursor.x;
-        let point_y = cursor.y;
 
+        let point_x = Math.max( Math.min( cursor.x - this.state.dragOffset.x, this.state.width * 0.95 ), this.state.width * 0.05 );
+        let point_y = Math.max( Math.min( cursor.y - this.state.dragOffset.y, this.state.depth * 0.95 ), this.state.width * 0.05 );
 
+        /*
+          roles : bl, tl, tr, br;
+        */
+
+        if( index === 1 )
+        /* left top corner */
         return {
           roomCoordinates:
           [
-            { x: state.roomCoordinates[ 0 ].x, y: state.roomCoordinates[ 0 ].y },
+            state.roomCoordinates[ 0 ],
             {
               x: state.roomCoordinates[ 0 ].x,
               y: point_y,
@@ -386,14 +389,88 @@ export class Editor extends React.Component {
               x : point_x,
               y : state.roomCoordinates[ state.roomCoordinates.length - 2 ].y
             },
-            { x: state.roomCoordinates[ state.roomCoordinates.length - 2 ].x, y: state.roomCoordinates[ state.roomCoordinates.length - 2 ].y },
+            state.roomCoordinates[ state.roomCoordinates.length - 2 ],
+            state.roomCoordinates[ state.roomCoordinates.length - 1 ],
+            // { x: state.roomCoordinates[ state.roomCoordinates.length - 2 ].x, y: state.roomCoordinates[ state.roomCoordinates.length - 2 ].y },
+            // { x: state.roomCoordinates[ state.roomCoordinates.length - 1 ].x, y: state.roomCoordinates[ state.roomCoordinates.length - 1 ].y },
+          ]
+        };
+
+        else if( index === 2 )
+        // top right corner
+        return {
+          roomCoordinates:
+          [
+            state.roomCoordinates[ 0 ],
+            state.roomCoordinates[ 1 ],
+            {
+              x: point_x,
+              y: state.roomCoordinates[ 1 ].y,
+            },
+            {
+              x : point_x,
+              y : point_y
+            },
+            {
+              x : state.roomCoordinates[ state.roomCoordinates.length - 1 ].x,
+              y : point_y
+            },
             { x: state.roomCoordinates[ state.roomCoordinates.length - 1 ].x, y: state.roomCoordinates[ state.roomCoordinates.length - 1 ].y },
           ]
         };
+
+        else if( index === 3 )
+        // bottom right corner
+        return {
+          roomCoordinates:
+          [
+            state.roomCoordinates[ 0 ],
+            state.roomCoordinates[ 1 ],
+            state.roomCoordinates[ 2 ],
+            {
+              x: state.roomCoordinates[ 2 ].x,
+              y: point_y,
+            },
+            {
+              x : point_x,
+              y : point_y
+            },
+            {
+              x : point_x,
+              y : state.roomCoordinates[ state.roomCoordinates.length - 1 ].y
+            },
+          ]
+        };
+
+        else if( index === 0 )
+        // bottom left corner
+        return {
+          roomCoordinates:
+          [
+            {
+              x: point_x,
+              y: state.roomCoordinates[ state.roomCoordinates.length - 1 ].y,
+            },
+            {
+              x : point_x,
+              y : point_y
+            },
+            {
+              x: state.roomCoordinates[ state.roomCoordinates.length - 3 ].x,
+              y: point_y,
+            },
+            state.roomCoordinates[ state.roomCoordinates.length - 3 ],
+            state.roomCoordinates[ state.roomCoordinates.length - 2 ],
+            state.roomCoordinates[ state.roomCoordinates.length - 1 ],
+          ]
+        };
+
+        // return coordinates;
       });
     };
     
-    const mouseup = (event) => {
+    const mouseup = ( event ) =>
+    {
       this.setState( ( state ) =>
       {
         return { states : [ ... state.states, state.roomCoordinates ] }
