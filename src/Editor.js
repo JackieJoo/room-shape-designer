f1d2f7132add9b11a8a7e46720b36b6a9b2c7232
Jack Shell <jackshel45@gmail.com>
2021-04-12 00:07:11 +0300

    Added drag of second point

--- src/Editor.js
+++ src/Editor.js
@@ -13,7 +13,7 @@ export class Editor extends React.Component
       corners : [],
       states :
       [
-        // initial state of the room, this points will have handlers for adding angles and cutouts
+        // initial state of the room
         [
           { x: 1000 * 0.05, y: 1000 * 0.95 },
           { x: 1000 * 0.05, y: 1000 * 0.05 },
@@ -77,12 +77,11 @@ export class Editor extends React.Component
     // push current state to the states array
 
     debugger
-    // const points = this.state.states[ 0 ];
     const points = this.state.roomCoordinates;
     const width = this.state.width;
     const depth = this.state.depth;
     // `${points[ 0 ].x},${points[ 0 ].y} ${points[ 1 ].x},${points[ 1 ].y} ${points[ 2 ].x},${points[ 2 ].y} ${points[ 3 ].x},${points[ 3 ].y}`
-    let pointsStr = points.map( ( el ) => `${el.x},${el.y}` ).join( ' ' );
+    let pointsStr = points.map( ( el ) => el !== null ? `${el.x},${el.y}` : '' ).join( ' ' );
     console.log( pointsStr );
     console.log( this.state );
     // console.log( this.state.roomCoordinates.toString() === this.state.states[ 0 ].toString() );
@@ -141,13 +140,13 @@ export class Editor extends React.Component
         <label>
           Pick the corner to change :
           <select value={this.state.corner} onChange={this.handleChangeCorner}>
-            <option value="bl">Bottom-left corner</option>
-            <option value="tl">Top-left corner</option>
-            <option value="tr">Top-right corner</option>
-            <option value="br">Bottom-right corner</option>
+            <option disabled={this.state.corners.indexOf( 'bl' ) !== -1} value="bl">Bottom-left corner</option>
+            <option disabled={this.state.corners.indexOf( 'tl' ) !== -1} value="tl">Top-left corner</option>
+            <option disabled={this.state.corners.indexOf( 'tr' ) !== -1} value="tr">Top-right corner</option>
+            <option disabled={this.state.corners.indexOf( 'br' ) !== -1} value="br">Bottom-right corner</option>
           </select>
         </label>
-        {/* <input type="submit" value="Submit" /> */}
+        <input type="submit" value="Submit" />
       </form>
       // {/* <div>Please pick a {this.state.action === 'square' || this.state.action === 'triag' ? 'corner' : 'wall' }</div> */}
     )
@@ -155,7 +154,6 @@ export class Editor extends React.Component
     /* */
     
     let prevCoordinate = points[ 0 ];
-    // let prevCoordinate2 = points[ 0 ];
     let form, changeWidth, changeDepth, displayButtons, pickCorner, dots;
 
     // if( !this.state.isReceivedWidth )
@@ -238,6 +236,7 @@ export class Editor extends React.Component
     {
       dots = points.map( ( el, i ) =>
       {
+        if( el !== null )
         return <rect x={ el.x - ( width * 0.04 / 2 ) } y={ el.y - ( width * 0.04 / 2 ) } width={ Math.min( width, depth ) * 0.04 } height={ Math.min( width, depth ) * 0.04 } fill="grey" onMouseDown={ ( e ) => this.dragSquareCorner( e, i, this.svgPolygon ) } />;
       });
     }
@@ -298,6 +297,20 @@ export class Editor extends React.Component
   handleSubmitCorner( event )
   {
     event.preventDefault();
+    this.setState( ( state ) =>
+    {
+      debugger;
+      if( JSON.stringify( state.roomCoordinates ) === JSON.stringify( state.states[ state.states.length - 1 ] ) ) /* nothing changed */
+      return state;
+      else
+      return {
+        states : [ ...state.states, state.roomCoordinates ],
+        // actions : [ ...state.actions, { [ state.corner ] : state.action } ],
+        actions : { ...state.actions, [ state.corner ] : state.action },
+        corners : [ ...state.corners, state.corner ],
+        states : [ ... state.states, state.roomCoordinates ]
+      }
+    })
   }
 
   /* */
@@ -329,10 +342,8 @@ export class Editor extends React.Component
 
   handleSubmitWidth( event )
   {
-    // clear form field after it has been submitted
     event.preventDefault();
     this.setState( ( state ) => ( { isReceivedWidth: true, roomCoordinates : this.calculateNewCoordinates( state ), states : [ this.calculateNewCoordinates( state ) ] } ) );
-    // console.log( this.state );
   }
 
   /* */
@@ -344,10 +355,8 @@ export class Editor extends React.Component
 
   handleSubmitDepth( event )
   {
-    // clear form field after it has been submitted
     event.preventDefault();
     this.setState( ( state ) => ( { isReceivedDepth: true, roomCoordinates : this.calculateNewCoordinates( state ), states : [ this.calculateNewCoordinates( state ) ] } ) );
-    // console.log( this.state );
   }
 
   // roomCoordinates :
@@ -404,10 +413,9 @@ export class Editor extends React.Component
         // if( index === 1 )
         if( this.state.corner === 'tl' )
         {
-          /* left top corner */
           // coordinates.splice
           // (
-          //   index,
+          //   1,
           //   0,
           //   {
           //     x: state.roomCoordinates[ 0 ].x,
@@ -424,25 +432,27 @@ export class Editor extends React.Component
           // )
           // console.log( 'coord', coordinates );
           // return { roomCoordinates : coordinates };
+          /* left top corner */
           return {
-            roomCoordinates:
-            [
-              state.roomCoordinates[ 0 ],
-              {
-                x: state.roomCoordinates[ 0 ].x,
-                y: point_y,
-              },
-              {
-                x : point_x,
-                y : point_y
-              },
-              {
-                x : point_x,
-                y : state.roomCoordinates[ state.roomCoordinates.length - 2 ].y
-              },
-              state.roomCoordinates[ state.roomCoordinates.length - 2 ],
-              state.roomCoordinates[ state.roomCoordinates.length - 1 ],
-            ]
+            roomCoordinates: calculateLeftTopCornerSquare( state, point_x, point_y )
+
+            // [
+            //   state.roomCoordinates[ 0 ],
+            //   {
+            //     x: state.roomCoordinates[ 0 ].x,
+            //     y: point_y,
+            //   },
+            //   {
+            //     x : point_x,
+            //     y : point_y
+            //   },
+            //   {
+            //     x : point_x,
+            //     y : state.roomCoordinates[ state.roomCoordinates.length - 2 ].y
+            //   },
+            //   state.roomCoordinates[ state.roomCoordinates.length - 2 ],
+            //   state.roomCoordinates[ state.roomCoordinates.length - 1 ],
+            // ]
           };
         }
         else if( this.state.corner === 'tr' )
@@ -523,67 +533,130 @@ export class Editor extends React.Component
     
     const mouseup = ( event ) =>
     {
-      this.setState( ( state ) =>
-      {
-        return { states : [ ... state.states, state.roomCoordinates ] }
-      })
+      // this.setState( ( state ) =>
+      // {
+      //   return { states : [ ... state.states, state.roomCoordinates ] }
+      // })
       document.removeEventListener( 'mousemove', mousemove );
       document.removeEventListener( 'mouseup', mouseup );
     };
-    
+
+    function calculateLeftTopCornerSquare( state, point_x, point_y )
+    {
+      if /* it's the first corner to be modified */
+      (
+        state.corners.length === 0
+        || (
+          state.corners.indexOf( 'tr' ) === -1
+          && state.corners.indexOf( 'br' ) === -1 
+          && state.corners.indexOf( 'bl' ) === -1
+        )
+      )
+      {
+        return [
+          state.roomCoordinates[ 0 ],
+          {
+            x: state.roomCoordinates[ 0 ].x,
+            y: point_y,
+          },
+          {
+            x : point_x,
+            y : point_y
+          },
+          {
+            x : point_x,
+            y : state.roomCoordinates[ state.roomCoordinates.length - 2 ].y
+          },
+          state.roomCoordinates[ state.roomCoordinates.length - 2 ],
+          state.roomCoordinates[ state.roomCoordinates.length - 1 ],
+        ]
+      }
+      else
+      {
+        let newLength = state.roomCoordinates.length;
+        let result = [];
+        let next = 0; /* element in the original array to take next */
+        // let corners = Object.keys( state.actions );
+        // let types = Object.values( state.actions );
+        // state.actions.forEach( ( el, i, arr ) => /* max length = 4. For each corner */
+        // {
+        debugger
+        if( state.actions.bl === undefined )
+        {
+          result.push( state.roomCoordinates[ 0 ] );
+          next = 1;
+        }
+        else if( state.actions.bl !== undefined )
+        {
+          if( state.actions.bl === 'square' )
+          {
+            result.push( state.roomCoordinates[ 0 ] );
+            result.push( state.roomCoordinates[ 1 ] );
+            result.push( state.roomCoordinates[ 2 ] );
+            next = 3;
+          }
+          /* Implement triag and cutout */
+          // return state.roomCoordinates;
+        }
+
+        result.push({ x: result[ result.length - 1 ].x, y: point_y });
+        result.push({ x: point_x, y: point_y });
+        result.push({ x: point_x, y: state.roomCoordinates[ next ].y });
+        next++;
+
+        // next += 2;
+
+        if( state.actions.tr === undefined )
+        {
+          result.push( state.roomCoordinates[ next ] );
+          next += 1;
+        }
+        else if( state.actions.tr !== undefined )
+        {
+          if( state.actions.tr === 'square' )
+          {
+            result.push( next++ );
+            result.push( next++ );
+            result.push( next++ );
+            // result.push( state.roomCoordinates[ 1 ] );
+            // result.push( state.roomCoordinates[ 2 ] );
+            next = 3;
+          }
+          /* Implement triag and cutout */
+          // return state.roomCoordinates;
+        }
+
+        if( state.actions.br === undefined )
+        {
+          // result.push({ x: point_x, y: state.roomCoordinates[ next ].y });
+          result.push( state.roomCoordinates[ next ] );
+          // next += 1;
+        }
+        else if( state.actions.br !== undefined )
+        {
+          if( state.actions.br === 'square' )
+          {
+            result.push( next++ );
+            result.push( next++ );
+            result.push( next++ );
+            // result.push( state.roomCoordinates[ 1 ] );
+            // result.push( state.roomCoordinates[ 2 ] );
+            next = 3;
+          }
+          /* Implement triag and cutout */
+          // return state.roomCoordinates;
+        }
+
+       return result;
+
+        // return state.roomCoordinates
+      }
+    }
+
     document.addEventListener( 'mousemove', mousemove );
     document.addEventListener( 'mouseup', mouseup );
+
   }
-
-  /* */
-
-  // drag(event, draggedElem)
-  // {
-  //   event.preventDefault();
-  //   let point = this.svg.createSVGPoint();
-  //   point.x = event.clientX;
-  //   point.y = event.clientY;
-  //   point = point.matrixTransform(this.svg.getScreenCTM().inverse());
-  //   this.setState({
-  //     dragOffset:
-  //     {
-  //       x: point.x - this.state.roomCoordinates[ 0 ].x,
-  //       y: point.y - this.state.roomCoordinates[ 0 ].y
-  //     }
-  //   });
-    
-  //   const mousemove = (event) =>
-  //   {
-  //     event.preventDefault();
-  //     point.x = event.clientX;
-  //     point.y = event.clientY;
-  //     let cursor = point.matrixTransform(this.svg.getScreenCTM().inverse());
-  //     this.setState( ( state ) =>
-  //     ({
-  //       roomCoordinates:
-  //       [
-  //         {
-  //           // x : cursor.x - this.state.dragOffset.x,
-  //           // y : cursor.y - this.state.dragOffset.y
-  //           x : Math.max( Math.min( cursor.x, state.width ), 0 ) - this.state.dragOffset.x,
-  //           y : Math.max( Math.min( cursor.y, state.depth ), 0 ) - this.state.dragOffset.y
-  //         },
-  //         { x: state.roomCoordinates[ 1 ].x, y: state.roomCoordinates[ 1 ].y },
-  //         { x: state.roomCoordinates[ 2 ].x, y: state.roomCoordinates[ 2 ].y },
-  //         { x: state.roomCoordinates[ 3 ].x, y: state.roomCoordinates[ 3 ].y },
-  //       ]
-  //     }));
-  //   };
-    
-  //   const mouseup = (event) => {
-  //     document.removeEventListener( 'mousemove', mousemove );
-  //     document.removeEventListener( 'mouseup', mouseup );
-  //   };
-    
-  //   document.addEventListener( 'mousemove', mousemove );
-  //   document.addEventListener( 'mouseup', mouseup );
-  // }
-
 }
 
 class Sample extends React.Component {
